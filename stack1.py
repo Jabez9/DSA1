bb800ccb6a61682a8fcbb87f7d42fe93bce48ec3
Jabez9 <mukoshijabez@gmail.com>
2025-02-27 15:42:33 +0300

    some more

--- stack1.py
+++ stack1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-#stack data structure
 
-class Stack():
+class Stack1():
     def __init__(self):
         self.items = []
 
@@ -17,6 +16,14 @@ class Stack():
     def peek(self):
         if not self.is_empty():
             return self.items[-1]
-    
+        
     def get_stack(self):
         return self.items
+    
+s = Stack1()
+s.push("A")
+s.push("B")
+s.push({"A":1, "B":2})
+s.push([1,2,3])
+s.peek()
+print(s.get_stack())
