afe518b871ed6dc9f9c22aab3c25aec265c35b5f
Jabez9 <mukoshijabez@gmail.com>
2025-03-15 20:27:55 +0300

    march15

--- Singly Linked List/mar15.py
+++ Singly Linked List/mar15.py
@@ -1,68 +1,64 @@
 #!/usr/bin/python3
+
 class Node:
     def __init__(self,data):
-        self.data = data
+        self.data = data 
         self.next = None
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self, val):
-        new_node = Node(val)
+    def append(self,value):
+        new_node = Node(value)
         if self.head is None:
             self.head = new_node
             return
+        
         last_node = self.head
         while last_node.next:
-            last_node=last_node.next 
+            last_node = last_node.next
+            last_node.next = new_node
 
-        last_node.next = new_node
+
+    def prepend(self,value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head= new_node
 
 
-    def prepend(self,input):
-        node1 = Node(input)
-        node1.next = self.head
-        self.head = node1
-
-    def findnode(self, value):
-        current = self.head
-        while current:
-            if current.data == value:
-                return current
-            current = current.next
-
+    def findnode(self,input):
+        cur = self.head
+        while cur:
+            if cur.data == input:
+                return cur
+            cur = cur.next
         return None
-    
+
     def insertafter(self,prev,data):
         prev_node = self.findnode(prev)
-        if  prev_node is None:
-            print(f"The previous node \"{prev}\" does not exits")
+        if prev_node is None:
+            print(f"The previous node \"{prev}\" does not exist.")
+
         else:
             new_node = Node(data)
             new_node.next = prev_node.next
-            prev_node.next= new_node
+            prev_node.next = new_node
 
-        
     def printlist(self):
         cur = self.head
         while cur:
-            print(cur.data, end='-->' if cur.next else "\n")
+            print(cur.data, end="->" if cur.next else "\n")
             cur = cur.next
 
+ll = LinkedList()
+ll.append("A")
+ll.append("B")
+ll.append("C")
+ll.append("D")
+ll.prepend("1")
+ll.insertafter("D", "2")
+ll.printlist()
 
-l = LinkedList()
 
-l.append("B")
-l.append("C")
-l.append("D")
-l.printlist()
-print("----------------")
-l.prepend("A")
-l.printlist()
-print("------------")
-l.insertafter("D", "E")
-l.printlist()
-print("-----------")
-# l.insertafter("F", "G")
-# l.printlist()
+
