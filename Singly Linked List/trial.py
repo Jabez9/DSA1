2100645d0cf0505229a362865124627b0f4ff3d7
Jabez9 <mukoshijabez@gmail.com>
2025-03-31 11:02:58 +0300

    m

--- Singly Linked List/trial.py
+++ Singly Linked List/trial.py
@@ -38,5 +38,86 @@ def merge_sorted(self, llist):
     self.head = new_head
     return self.head
 
+from mar26 import length_recursive
+def find_nth_from_last(self, n):
+    total = self.length_recursive()
+
+    cur = self.head
+    while cur:
+        if total == n:
+            print(cur.data)
+            return cur.data
+        total -=1
+        cur = cur.next
+
+    if not cur:
+        return
+    
+def get_nth_from_last(self,n):
+    p = self.head
+    q = self.head
+
+    if n > 0:
+        count = 0
+        while p:
+            count +=1
+            if count >= n:
+                break
+            p = p.next
+
+        if not p:
+            print(n, " is not in the range of the list")
+            return
+        
+        else:
+            while q and p.next:
+                q = q.next
+                p  = p.next
+            # return q.data
+        
+            while q:
+                print(q.data, end="-->")
+                q = q.next
+            print() 
+
+        
+    else:
+        return "Invalid n"
+    
+
+def get_nth_to_last(self,n):
+    p = self.head
+    q = self.head
+
+    if n > 0:
+        count = 0
+        while p:
+            count +=1
+            if count >= n:
+                break
+            p = p.next
+
+        if not p:
+            print(n, " is not in the range of the list")
+            return
+        
+        else:
+            while q and p.next:
+                q = q.next
+                p  = p.next
+            # return q.data
+        
+            while q:
+                print(q.data, end="-->")
+                q = q.next
+            print() 
+
+    else:
+        return "Invalid n"
+
+
+
+    
+
 
         
